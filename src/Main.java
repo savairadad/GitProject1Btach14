c7e099fa2dca6c33b0bcc2884b45ddb17c48151b
savairadad <savairadadmohammad@gmail.com>
2022-12-20 15:22:07 -0500

    version2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("First Version Is complete");
+        System.out.println(" Version Is complete");
     }
 }
